7509c2444e757986cddf310015ccdb8efec61d06
Ben Robinson <microsoft@ben-robinson.com>
2022-08-09 14:19:27 +0100

    Remove logging

--- dist/index.js
+++ dist/index.js
@@ -1102,9 +1102,7 @@ function run() {
                 'last_deployed_time',
             ];
             //Apply days_to_keep
-            core.info('days-to-keep: ' + daysToKeep);
             if (daysToKeep && daysToKeep > 0) {
-                core.info('will push filter');
                 let dateDiff = new Date().getDate() - daysToKeep;
                 let deleteBefore = new Date(new Date().setDate(dateDiff));
                 let deleteBeforeString = deleteBefore.toISOString().split('T')[0];
